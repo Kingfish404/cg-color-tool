d626c5e9cee99764b4377ac2b3053b8bef8a4c01
Kingfish404 <jinyu929@qq.com>
2022-01-02 19:16:54 +0800

    feat: add input type color support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,48 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from '../styles/index.module.scss'
 
 const regHex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/g;
-const regRGB = /^(\d|(\d{2})|(1\d{2})|(2[0-5]{2})),(\d|(\d{2})|(1\d{2})|(2[0-5]{2})),(\d|(\d{2})|(1\d{2})|(2[0-5]{2}))$/g;
-const regRG = /^(0|1|(0\.\d+)),(0|1|(0\.\d+)),(0|1|(0\.\d+))$/g
+const regRGBValue = /^(\d|(\d{2})|(1\d{2})|(2[0-5]{2})),(\d|(\d{2})|(1\d{2})|(2[0-5]{2})),(\d|(\d{2})|(1\d{2})|(2[0-5]{2}))$/g;
+const regRGValue = /^(0|1|(0\.\d+)),(0|1|(0\.\d+)),(0|1|(0\.\d+))$/g
 
 function rgbToHexArr(rgb: string): Array<string> {
-  const arr: Array<number> = rgb.substring(4, rgb.length - 1).split(',').map(v => (Number(v)));
-  const out: Array<string> = new Array(3).fill("");
-  arr.forEach((v, index) => {
-    let nv = v.toString(16);
-    if (nv.length == 1) {
-      nv = "0" + v;
-    }
-    out[index] = nv;
-  });
-  return out;
+  if (!rgb.substring(4, rgb.length - 1).match(regRGBValue)) {
+    return ["ff", "ff", "ff"];
+  }
+  const arr: Array<string> = rgb
+    .substring(4, rgb.length - 1)
+    .split(",")
+    .map((v) => {
+      const n = parseInt(v).toString(16);
+      return n.length == 1 ? n + n : n;
+    });
+  return arr;
 }
 
 function hexToRgbArr(hex: string): Array<number> {
-  const result = /^#?([A-Za-f\d]{1,2})([A-Za-f\d]{1,2})([A-Za-f\d]{1,2})/i.exec(hex);
-  let out = [];
-  if (!result) {
+  if (!hex.match(regHex)) {
     return [255, 255, 255];
-  } else if (result[1].length === 1) {
-    out = result.map(v => (v + v));
-  } else {
-    out = result;
   }
-  return [parseInt(out[1], 16), parseInt(out[2], 16), parseInt(out[3], 16)];
+  const result = hex.length == 4 ?
+    hex.substring(1, 4).split("").map(v => v + v) :
+    [hex.substring(1, 3), hex.substring(3, 5), hex.substring(5)];
+  return [parseInt(result[0], 16), parseInt(result[1], 16), parseInt(result[2], 16)];
 }
 
 const Home: NextPage = () => {
-  const [backgroundColor, setBackgroundColor] = useState('#66ccff');
-  const [backgroundColorNext, setBackgroundColorNext] = useState('');
+  const [backgroundColorRGB, setBackgroundColorRGB] = useState("rgb(102,204,255)");
+  const [backgroundColorHex, setBackgroundColorHex] = useState('');
   const [backgroundColorRG, setBackgroundColorRG] = useState('');
   const [color, setColor] = useState('#111');
 
   useEffect(() => {
-    let rgbArr;
-    if (backgroundColor[0] === '#') {
-      rgbArr = hexToRgbArr(backgroundColor);
-      setBackgroundColorNext(`rgb(${rgbArr[0]},${rgbArr[1]},${rgbArr[2]})`);
-    } else {
-      const hexArr = rgbToHexArr(backgroundColor);
-      rgbArr = hexToRgbArr(`#${hexArr[0]}${hexArr[1]}${hexArr[2]})`);
-      setBackgroundColorNext(`#${hexArr[0]}${hexArr[1]}${hexArr[2]})`);
-    }
-
-    setBackgroundColorRG(`rg(${rgbArr[0] / 255},${rgbArr[1] / 255},${rgbArr[2] / 255})`);
+    const hexArr = rgbToHexArr(backgroundColorRGB);
+    const hexStr = `#${hexArr.join("")}`;
+    const rgbArr = backgroundColorRGB.substring(4, backgroundColorRGB.length - 1).split(",").map(v => parseFloat(v));
+    setBackgroundColorHex(hexStr);
+    setBackgroundColorRG(`rg(${(rgbArr.map(v => Math.round(v / 255 * 1000) / 1000).join(","))})`);
 
     if ((rgbArr[0] + rgbArr[1] + rgbArr[2]) < 270) {
       setColor('#eee');
@@ -59,7 +51,7 @@ const Home: NextPage = () => {
     }
 
     return () => { }
-  }, [backgroundColor])
+  }, [backgroundColorRGB]);
 
   return (
     <div className={styles.container}>
@@ -69,73 +61,74 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main
+      <main className={styles.main}
         style={{
-          backgroundColor: backgroundColor,
-          color: color
-        }}
-        className={styles.main}>
-        <div
-          style={{
-            color: color
-          }}
-          className={styles.label}>
-          <span>{backgroundColor}</span>
-          <span>{backgroundColorNext}</span>
+          ['--color' as any]: color,
+          backgroundColor: backgroundColorRGB,
+        }}>
+        <header>
+          <h1>Color Tool</h1>
+        </header>
+        <div className={styles.label}>
+          <span>{backgroundColorRGB}</span>
+          <span>{backgroundColorHex}</span>
           <br />
           <span>{backgroundColorRG}</span>
         </div>
-        <input
-          style={{
-            color: color
-          }}
-          id='hex' type="text"
-          placeholder='hex'
-          onChange={(e) => {
-            const value = e.target.value.charAt(0) === '#' ?
-              e.target.value :
-              `#${e.target.value}`;
-            if (value.match(regHex)) {
-              setBackgroundColor(value);
-            };
-          }}
-        />
-        <input
-          style={{
-            color: color
-          }}
-          id='rgb' type="text"
-          placeholder='rgb'
-          onChange={(e) => {
-            const value = e.target.value;
-            if (value.match(regRGB)) {
-              setBackgroundColor(`rgb(${value})`);
-            };
-          }}
-        />
-        <input
-          style={{
-            color: color
-          }}
-          id='rg' type="text"
-          placeholder='rg'
-          onChange={(e) => {
-            const value = e.target.value;
-            if (value.match(regRG)) {
-              const arr = value.split(',').map(v => (Number(v) * 255));
-              const v = arr.join(',');
-              setBackgroundColor(`rgb(${v})`);
-            };
-          }}
-        />
+        <div>
+          <input className={styles.colorPicker}
+            type="color"
+            value={(() => {
+              const hexArr = rgbToHexArr(backgroundColorRGB);
+              const hex = `#${hexArr.join('')}`;
+              return hex;
+            })()}
+            onChange={(e) => {
+              const value = e.target.value;
+              const rgbArr = hexToRgbArr(value);
+              setBackgroundColorRGB(`rgb(${rgbArr.join(',')})`);
+            }} />
+          <input
+            id='hex' type="text"
+            placeholder='hex'
+            onChange={(e) => {
+              const value = e.target.value.charAt(0) === '#' ?
+                e.target.value :
+                `#${e.target.value}`;
+              if (value.match(regHex)) {
+                const rgbArr = hexToRgbArr(value);
+                setBackgroundColorRGB(`rgb(${rgbArr.join(',')}`);
+              };
+            }}
+          />
+          <input
+            id='rgb' type="text"
+            placeholder='rgb'
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value.match(regRGBValue)) {
+                setBackgroundColorRGB(`rgb(${value})`);
+              };
+            }}
+          />
+          <input
+            id='rg' type="text"
+            placeholder='rg'
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value.match(regRGValue)) {
+                const arr = value.split(',').map(v => (parseFloat(v) * 255));
+                const v = arr.join(',');
+                setBackgroundColorRGB(`rgb(${v})`);
+              };
+            }}
+          />
+        </div>
         <div
-          style={{
-            color: color
-          }}
         >
           <p>hex is like #66ccff or #6cf</p>
           <p>rgb is like rgb(102,204,255)</p>
-          <p>rg is like rg(0.182,0.364,0.455)</p>
+          <p>rg is like rg(0.4,0.8,1)</p>
           <p>just input 6cf or 102,204,255</p>
         </div>
         <footer
